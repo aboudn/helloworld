366c26c2b015ec2ca4f658436fe4458dab8ac966
aboudn <niazigr@gmail.com>
2021-09-05 14:16:09 +0200

    noch mal

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome  Vue.js App"/>
+    <HelloWorld msg="Welcome  to your Vue.js App"/>
   </div>
 </template>
 
